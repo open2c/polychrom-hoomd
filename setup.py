88b0358bbe844542f21af7ef1443fb3222243003
Maxime Tortora <maxime.tortora@icloud.com>
2024-10-02 16:34:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
 import os
 import io
+
 from setuptools import setup, find_packages
+
 
 VERSION = "0.1.0"
 DESCRIPTION = "HOOMD-blue-based package for coarse-grained polymer and chromosome simulations."
 
-
-def _read(*parts, **kwargs):
-    filepath = os.path.join(os.path.dirname(__file__), *parts)
-    encoding = kwargs.pop("encoding", "utf-8")
-    with io.open(filepath, encoding=encoding) as fh:
-        text = fh.read()
-    return text
-
-
-def get_requirements(path):
-    content = _read(path)
-    return [
-        req
-        for req in content.split("\n")
-        if req != "" and not (req.startswith("#") or req.startswith("-"))
-    ]
-
-install_requires = get_requirements("requirements.txt")
 
 setup(
     name="polychrom-hoomd",
@@ -32,6 +16,5 @@ setup(
     author="Open2C",
     author_email="open.chromosome.collective@gmail.com",
     license="MIT",
-    packages=find_packages(),
-    install_requires=install_requires
+    packages=find_packages()
 )
