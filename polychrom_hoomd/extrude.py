d860d2ad43d217cd778ea6085d34576b6c24b8aa
mtortora <maxime.tortora@icloud.com>
2024-12-04 12:24:04 -0800

    Implemented CUDA-accelerated 3D extrusion

--- polychrom_hoomd/extrude.py
+++ polychrom_hoomd/extrude.py
@@ -2,50 +2,52 @@ import os
 import hoomd
 import warnings
 
-import numpy as np
 import polychrom_hoomd.utils as utils
 
 try:
-    import cupy as cp
+    import cupy as xp
     dpath = os.path.dirname(os.path.abspath(__file__))
-    
+        
     with open(f'{dpath}/kernels/lef_neighbor_search.cuh', 'r') as cuda_file:
         cuda_code = cuda_file.read()
-        cuda_module = cp.RawModule(code=cuda_code)
+        cuda_module = xp.RawModule(code=cuda_code)
     
     _single_leg_search = cuda_module.get_function('_single_leg_search')
     
 except ImportError:
+    import numpy as xp
     warnings.warn("Could not load CuPy library - local/3D topology updates unavailable")
 
 
 def update_topology(system, bond_list, local=True, thermalize=False):
-    """Update topology on either GPU or CPU, based on availability"""
+    """Update topology on either GPU or xpU, based on availability"""
 
     LEF_typeid = system.state.bond_types.index('LEF')
     LEF_dummy_typeid = system.state.bond_types.index('LEF_dummy')
     
     if len(bond_list) > 0:
         # Discard contiguous loops
-        bond_array = np.asarray(bond_list, dtype=np.uint32)
-        type_array = np.ones(len(bond_array),  dtype=np.uint32) * LEF_typeid
+		
+        bond_array = xp.asarray(bond_list, dtype=xp.uint32)
+        type_array = xp.ones(len(bond_array),  dtype=xp.uint32) * LEF_typeid
 
         redundant_bonds = (bond_array[:, 1] - bond_array[:, 0] < 1)
 		
-        bond_array[redundant_bonds] = np.asarray([0, 1], dtype=np.uint32)
+        bond_array[redundant_bonds] = xp.asarray([0, 1], dtype=xp.uint32)
         type_array[redundant_bonds] = LEF_dummy_typeid
 
     else:
-        bond_array = np.empty(0, dtype=np.uint32)
-        type_array = np.empty(0, dtype=np.uint32)
+        bond_array = xp.empty(0, dtype=xp.uint32)
+        type_array = xp.empty(0, dtype=xp.uint32)
 
     if local:
         try:
             _update_topology_local(system, bond_array, type_array, LEF_typeid, LEF_dummy_typeid)
+            warnings.warn("Using local topology update")
 
         except Exception as e:
-            warnings.warn("%s - reverting to non-local topology update" % e)
             _update_topology_nonlocal(system, bond_array, type_array, LEF_typeid, LEF_dummy_typeid)
+            warnings.warn("%s - reverting to non-local topology update" % e)
 
     else:
         _update_topology_nonlocal(system, bond_array, type_array, LEF_typeid, LEF_dummy_typeid)
@@ -55,31 +57,34 @@ def update_topology(system, bond_list, local=True, thermalize=False):
 
 
 def _update_topology_nonlocal(system, bond_array, type_array, type_id, dummy_id):
-    """Update topology on the CPU"""
+    """Update topology on the xpU"""
     
     snap = system.state.get_snapshot()
     snap_gsd = utils.get_gsd_snapshot(snap)
 
-    non_LEF_ids = (snap.bonds.typeid != type_id)*(snap.bonds.typeid != dummy_id)
+    non_LEF_ids = xp.asarray((snap.bonds.typeid != type_id)*(snap.bonds.typeid != dummy_id))
 		    
     n_LEF = bond_array.shape[0]
-    n_non_LEF = np.count_nonzero(non_LEF_ids)
+    n_non_LEF = int(xp.count_nonzero(xp.asarray(non_LEF_ids)))
         
-    groups = np.zeros((n_non_LEF+n_LEF, 2), dtype=np.uint32)
-    typeids = np.zeros(n_non_LEF+n_LEF, dtype=np.uint32)
+    groups = xp.zeros((n_non_LEF+n_LEF, 2), dtype=xp.uint32)
+    typeids = xp.zeros(n_non_LEF+n_LEF, dtype=xp.uint32)
     
-    groups[:n_non_LEF] = snap.bonds.group[non_LEF_ids]
-    typeids[:n_non_LEF] = snap.bonds.typeid[non_LEF_ids]
+    group_array = xp.asarray(snap.bonds.group, dtype=xp.uint32)
+    typeid_array = xp.asarray(snap.bonds.typeid, dtype=xp.uint32)
+
+    groups[:n_non_LEF] = group_array[non_LEF_ids]
+    typeids[:n_non_LEF] = typeid_array[non_LEF_ids]
 
     if n_LEF:
-        groups[n_non_LEF:] = bond_array
-        typeids[n_non_LEF:] = type_array
+        groups[n_non_LEF:] = xp.asarray(bond_array)
+        typeids[n_non_LEF:] = xp.asarray(type_array)
 
     # Bond resizing in HOOMD v3 requires full array reassignment
     snap_gsd.bonds.N = n_non_LEF + n_LEF
 
-    snap_gsd.bonds.group = groups
-    snap_gsd.bonds.typeid = typeids
+    snap_gsd.bonds.group = groups.get() if xp.__name__ == 'cupy' else groups
+    snap_gsd.bonds.typeid = typeids.get() if xp.__name__ == 'cupy' else typeids
 
     # Configuration step/box data requires manual setting as of gsd v2.8
     snap_gsd.configuration.step = system.timestep
@@ -95,14 +100,14 @@ def _update_topology_local(system, bond_array, type_array, type_id, dummy_id):
     """Update topology locally on the GPU"""
     		
     with system.state.gpu_local_snapshot as local_snap:
-        bond_ids = cp.array(local_snap.bonds.typeid, copy=False)
+        bond_ids = xp.asarray(local_snap.bonds.typeid)
 
-        is_bound = cp.equal(bond_ids, type_id)
-        is_unbound = cp.equal(bond_ids, dummy_id)
+        is_bound = xp.equal(bond_ids, type_id)
+        is_unbound = xp.equal(bond_ids, dummy_id)
 
-        is_LEF = cp.logical_or(is_bound, is_unbound)
+        is_LEF = xp.logical_or(is_bound, is_unbound)
 
-        if bond_array.shape[0] == type_array.shape[0] == cp.count_nonzero(is_LEF):
+        if bond_array.shape[0] == type_array.shape[0] == xp.count_nonzero(is_LEF):
             local_snap.bonds.group[is_LEF] = bond_array
             local_snap.bonds.typeid[is_LEF] = type_array
 
@@ -110,39 +115,29 @@ def _update_topology_local(system, bond_array, type_array, type_id, dummy_id):
             raise RuntimeError("Unable to dynamically resize bond arrays on the GPU")
 
 
-def update_topology_3D(system, neighbor_list, leg_off_rate, threads_per_block=256):
+def update_topology_3D(system, neighbor_list, leg_off_rate, cutoff, threads_per_block=256):
     """Attempt stochastic 3D cohesin moves on the GPU"""
 
     LEF_typeid = system.state.bond_types.index('LEF')
-
+    backbone_typeid = system.state.bond_types.index('Backbone')
+    
     with system.state.gpu_local_snapshot as local_snap:
-        bond_ids = cp.array(local_snap.bonds.typeid, copy=False)
-        is_bound = cp.equal(bond_ids, LEF_typeid)
+        bond_ids = xp.asarray(local_snap.bonds.typeid)
+        anchors = _get_lef_anchors(bond_ids, LEF_typeid, leg_off_rate)
         
         N = int(bond_ids.size)
-        N_bound = int(cp.count_nonzero(is_bound))
-        
-        rng_left = cp.random.random(N_bound, dtype=cp.float32)
-        rng_right = cp.random.random(N_bound, dtype=cp.float32)
-        
-        unbind_left = cp.less(rng_left, leg_off_rate)
-        unbind_right = cp.less(rng_right, leg_off_rate)
-        
-        unbind = cp.logical_and(unbind_left, unbind_right)
-
-        unbind_left = cp.logical_and(unbind_left, cp.logical_not(unbind))
-        unbind_right = cp.logical_and(unbind_right, cp.logical_not(unbind))
-        
-        anchors = cp.zeros(N, dtype=cp.int32)
-        rng = cp.random.random(N, dtype=cp.float32)
-        
-        anchors[is_bound] = cp.where(unbind_right, 1, anchors[is_bound])
-        anchors[is_bound] = cp.where(unbind_left, -1, anchors[is_bound])
+        rng = xp.random.random(N, dtype=xp.float32)
 
         groups = local_snap.bonds.group._coerce_to_ndarray()
         tags = local_snap.particles.tag._coerce_to_ndarray()
         rtags = local_snap.particles.rtag._coerce_to_ndarray()
+
+        is_backbone = xp.equal(bond_ids, backbone_typeid)
+        backbone_ids = groups[is_backbone]
         
+        N_min = int(xp.amin(backbone_ids))
+        N_max = int(xp.amax(backbone_ids))
+
         with neighbor_list.gpu_local_nlist_arrays as data:
             nlist = data.nlist._coerce_to_ndarray()
             n_neigh = data.n_neigh._coerce_to_ndarray()
@@ -153,5 +148,33 @@ def update_topology_3D(system, neighbor_list, leg_off_rate, threads_per_block=25
             _single_leg_search(
                 (num_blocks,),
                 (threads_per_block,),
-                (N, nlist, n_neigh, head_list, tags, rtags, anchors, rng, groups)
+                (N, N_min, N_max, cutoff,
+                 nlist, n_neigh, head_list, tags, rtags,
+                 anchors, rng,
+                 groups)
             )
+
+
+def _get_lef_anchors(bond_ids, type_id, leg_off_rate):
+    """Stochastically unbind individual cohesin legs on the GPU"""
+
+    is_bound = xp.equal(bond_ids, type_id)
+    N_bound = int(xp.count_nonzero(is_bound))
+        
+    rng_left = xp.random.random(N_bound, dtype=xp.float32)
+    rng_right = xp.random.random(N_bound, dtype=xp.float32)
+        
+    unbind_left = xp.less(rng_left, leg_off_rate)
+    unbind_right = xp.less(rng_right, leg_off_rate)
+        
+    unbind = xp.logical_and(unbind_left, unbind_right)
+
+    unbind_left = xp.logical_and(unbind_left, xp.logical_not(unbind))
+    unbind_right = xp.logical_and(unbind_right, xp.logical_not(unbind))
+	
+    anchors = xp.zeros(int(bond_ids.size), dtype=xp.int32)
+	
+    anchors[is_bound] = xp.where(unbind_right, 1, anchors[is_bound])
+    anchors[is_bound] = xp.where(unbind_left, -1, anchors[is_bound])
+    
+    return anchors
